460032a952f5a1c816a03520aab141eca225fc93
Clementina Ulombe <clementinaulombe92@gmail.com>
2025-11-11 21:03:39 +0100

    H

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -94,7 +94,7 @@ export default function UsersPage() {
   
       }
   
-      setFormErrors(prev => ({ ...prev, [name]: error }));
+      setFormErrors((prev: any) => ({ ...prev, [name]: error }));
   
       return error;
   
