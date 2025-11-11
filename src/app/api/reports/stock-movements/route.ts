b7c883b22dad89407a9b22974995ca51f9bf9af5
Clementina Ulombe <clementinaulombe92@gmail.com>
2025-11-11 10:48:53 +0100

    refactor:Corrigi o erro de tipagem no ficheiro src/app/api/reports/stock-movements/route.ts que estava a causar a falha    na compilação.

--- src/app/api/reports/stock-movements/route.ts
+++ src/app/api/reports/stock-movements/route.ts
@@ -8,7 +8,7 @@ const prisma = new PrismaClient();
 export async function GET(req: Request) {
   const session = await getServerSession(authOptions);
 
-  if (!session || !session.user || ![UserRole.ADMIN, UserRole.STOCKIST].includes(session.user.role as UserRole)) {
+  if (!session || !session.user || !([UserRole.ADMIN, UserRole.STOCKIST] as UserRole[]).includes(session.user.role as UserRole)) {
     return new NextResponse(JSON.stringify({ error: 'Acesso não autorizado' }), { status: 403 });
   }
 
